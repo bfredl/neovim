41c7471e8a34ad97fd290625542ea290104fcbda
Björn Linse <bjorn.linse@gmail.com>
2015-09-14 13:10:51 +0200

    encoding: update tests

--- test/functional/ex_cmds/encoding_spec.lua
+++ test/functional/ex_cmds/encoding_spec.lua
@@ -15,27 +15,16 @@ describe('&encoding', function()
     execute('set encoding=latin1')
     -- error message expected
     feed('<cr>')
-    neq(nil, string.find(eval('v:errmsg'), '^E905:'))
     eq('utf-8', eval('&encoding'))
     -- check nvim is still in utf-8 mode
     eq(3, eval('strwidth("Bär")'))
   end)
 
-  it('can be changed before startup', function()
+  it('cannot be changed before startup', function()
     clear('set enc=latin1')
-    execute('set encoding=utf-8')
-    -- error message expected
     feed('<cr>')
-    eq('latin1', eval('&encoding'))
-    eq(4, eval('strwidth("Bär")'))
-  end)
-
-  it('is not changed by `set all&`', function()
-    -- we need to set &encoding to something non-default. Use 'latin1'
-    clear('set enc=latin1')
-    execute('set all&')
-    eq('latin1', eval('&encoding'))
-    eq(4, eval('strwidth("Bär")'))
+    eq('utf-8', eval('&encoding'))
+    eq(3, eval('strwidth("Bär")'))
   end)
 
 end)
