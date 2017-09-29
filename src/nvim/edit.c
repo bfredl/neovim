0a54f241b6b3c008123e40d3b83b622acea87cc9
Timothy C Eichler <timeyyy_da_man@hotmail.com>
2017-09-29 16:19:40 +0200

    extmarks: work when autoindent triggers

--- src/nvim/edit.c
+++ src/nvim/edit.c
@@ -1779,6 +1779,10 @@ change_indent (
 
     xfree(new_line);
   }
+
+  // Move extmarks
+  extmark_col_adjust(curbuf, curwin->w_cursor.lnum, 0, 1L, (long)amount,
+                     kExtmarkUndo);
 }
 
 /*
