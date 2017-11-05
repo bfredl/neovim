65cc7064f0f7a24590131c79d39a752e627436c9
Björn Linse <bjorn.linse@gmail.com>
2017-11-05 10:11:03 +0100

    remove two-iteration

--- src/nvim/screen.c
+++ src/nvim/screen.c
@@ -421,29 +421,42 @@ void update_screen(int type)
   did_one = FALSE;
   search_hl.rm.regprog = NULL;
 
-  for (int floating = 0; floating < 2; floating++) {
-    FOR_ALL_WINDOWS_IN_TAB(wp, curtab) {
-      if (wp->w_floating != floating) {
-        continue;
-      }
-      if (floating && did_one) {
-        // TODO: be a lot more precise
-        wp->w_redr_type = NOT_VALID;
-      }
-
-      if (wp->w_redr_type != 0) {
-        if (!did_one) {
-          did_one = TRUE;
-          start_search_hl();
-        }
-        win_update(wp);
-      }
-
-      /* redraw status line after the window to minimize cursor movement */
-      if (wp->w_redr_status) {
-        win_redr_status(wp);
-      }
+  FOR_ALL_WINDOWS_IN_TAB(wp, curtab) {
+    if (wp->w_floating) {
+      continue;
     }
+
+    if (wp->w_redr_type != 0) {
+      if (!did_one) {
+        did_one = TRUE;
+        start_search_hl();
+      }
+      win_update(wp);
+    }
+
+    /* redraw status line after the window to minimize cursor movement */
+    if (wp->w_redr_status) {
+      win_redr_status(wp);
+    }
+  }
+
+  FOR_ALL_WINDOWS_IN_TAB(wp, curtab) {
+    if (!wp->w_floating) {
+      continue;
+    }
+    if (did_one) {
+      // TODO: be a lot more precise
+      wp->w_redr_type = NOT_VALID;
+    }
+
+    if (wp->w_redr_type != 0) {
+      if (!did_one) {
+        did_one = TRUE;
+        start_search_hl();
+      }
+      win_update(wp);
+    }
+
   }
   end_search_hl();
   // May need to redraw the popup menu.
