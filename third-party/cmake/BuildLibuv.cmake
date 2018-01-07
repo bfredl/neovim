57a95e268ed531d9a2343021f74df7fe8ee406e9
Björn Linse <bjorn.linse@gmail.com>
2018-01-07 09:43:55 +0100

    TEST: build libuv with ASAN

--- third-party/cmake/BuildLibuv.cmake
+++ third-party/cmake/BuildLibuv.cmake
@@ -39,7 +39,8 @@ endfunction()
 set(UNIX_CFGCMD sh ${DEPS_BUILD_DIR}/src/libuv/autogen.sh &&
   ${DEPS_BUILD_DIR}/src/libuv/configure --with-pic --disable-shared
   --prefix=${DEPS_INSTALL_DIR} --libdir=${DEPS_INSTALL_DIR}/lib
-  CC=${DEPS_C_COMPILER})
+  CC=clang CFLAGS=-fno-sanitize-recover=all\ -fno-omit-frame-pointer\ -fno-optimize-sibling-calls\ -fsanitize=address\ -fsanitize=undefined
+)
 
 if(UNIX)
   BuildLibuv(
