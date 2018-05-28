200166e3e630bc763b0d76feeb1ed6fc0362ce4f
Utkarsh Maheshwari <utkarshme96@gmail.com>
2018-05-28 23:40:56 +0530

    Change ui_line to use window grids and not global grid

--- src/nvim/ui.c
+++ src/nvim/ui.c
@@ -315,11 +315,14 @@ void ui_set_ext_option(UI *ui, UIExtension ext, bool active)
   }
 }
 
-void ui_line(int row, int startcol, int endcol, int clearcol, int clearattr)
+void ui_line(ScreenGrid *grid, int row, int startcol, int endcol, int clearcol, int clearattr)
 {
-  size_t off = LineOffset[row]+(size_t)startcol;
-  UI_CALL(raw_line, 1, row, startcol, endcol, clearcol, clearattr,
-          (const schar_T *)ScreenLines+off, (const sattr_T *)ScreenAttrs+off);
+  size_t off = grid->LineOffset[row] + (size_t)startcol;
+  UI_CALL(raw_line, 1, grid->OffsetRow + row, grid->OffsetColumn + startcol,
+          grid->OffsetColumn + endcol, grid->OffsetColumn + clearcol,
+          clearattr, (const schar_T *)grid->ScreenLines + off,
+          (const schar_T *)grid->ScreenAttrs + off);
+
   if (p_wd) {  // 'writedelay': flush & delay each time.
     int old_row = row, old_col = col;
     // If'writedelay is active, we set the cursor to highlight what was drawn
