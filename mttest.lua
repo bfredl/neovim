46f5cfbe40706077b7400fbba9cda70d3b4e96f5
Björn Linse <bjorn.linse@gmail.com>
2019-11-18 18:44:11 +0100

    NEWITER

--- mttest.lua
+++ mttest.lua
@@ -10,15 +10,16 @@ typedef struct mttree_s MarkTree;
 typedef struct mtnode_s mtnode_t;
 
 typedef struct {
-  // TODO(bfredl): really redundant with "backlinks", but keep now so we can do
-  // consistency check easily.
-  mtnode_t *x;
+  int oldcol;
   int i;
-} mtpos_t;
+} iterstate_t;
 
 typedef struct {
   mtkey_t pos;
-  mtpos_t stack[64], *p;
+  int lvl;
+  mtnode_t *node;
+  int i;
+  iterstate_t s[64];
 } MarkTreeIter;
 
 
@@ -26,7 +27,8 @@ MarkTree *marktree_new(bool rel);
 void marktree_put(MarkTree *b, mtkey_t k);
 void marktree_put_pos(MarkTree *b, int row, int col, uint64_t id);
 int marktree_itr_get(MarkTree *b, mtkey_t k, MarkTreeIter *itr);
-int marktree_itr_next(MarkTree *b, MarkTreeIter *itr);
+void marktree_itr_first(MarkTree *b, MarkTreeIter *itr);
+bool marktree_itr_next(MarkTree *b, MarkTreeIter *itr);
 int marktree_itr_prev(MarkTree *b, MarkTreeIter *itr);
 mtkey_t marktree_itr_test(MarkTreeIter *itr);
 char *mt_inspect_rec(MarkTree *b);
@@ -34,9 +36,9 @@ char *mt_inspect_rec(MarkTree *b);
 
 
 p = require'luadev'.print
-tree = ffi.C.marktree_new(1)
+tree = ffi.C.marktree_new(0)
 iter = ffi.new("MarkTreeIter[1]")
-for i = 1,400 do
+for i = 1,30 do
   ffi.C.marktree_put_pos(tree, 1, i, i)
 end
 ss = ffi.C.mt_inspect_rec(tree)
@@ -46,16 +48,21 @@ raa()
 
 key = ffi.new("mtkey_t")
 key.row = -1
-ffi.C.marktree_itr_get(tree, key, iter)
+--ffi.C.marktree_itr_get(tree, key, iter)
+ffi.C.marktree_itr_first(tree, iter)
 i = 1
-while ffi.C.marktree_itr_next(tree, iter) > 0 do
-  val = ffi.C.marktree_itr_test(iter)
-  p(val.row, val.col, val.id)
+repeat
+
+  val = ffi.C.marktree_itr_test(iter) p(val.row, val.col, val.id) p(ffi.C.marktree_itr_next(tree, iter))
+  --iter[0].lvl
+
   if false and val.col ~= i then
     error("x")
   end
   i = i +1
-end
+until not ffi.C.marktree_itr_next(tree, iter)
+
+ee()
 
 --val.id
 
