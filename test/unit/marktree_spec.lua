a3a21264540b01fb31714ff335445d101db18de6
Björn Linse <bjorn.linse@gmail.com>
2019-12-02 20:13:45 +0100

    shadoworder

--- test/unit/marktree_spec.lua
+++ test/unit/marktree_spec.lua
@@ -7,20 +7,59 @@ local ok      = helpers.ok
 
 local lib = helpers.cimport("./src/nvim/marktree.h")
 
+local function tablelength(t)
+  local count = 0
+  for _ in pairs(t) do count = count + 1 end
+  return count
+end
+
+local function shadoworder(tree, shadow, iter)
+  ok(iter ~= nil)
+  local status = lib.marktree_itr_first(tree, iter)
+  local count = 0
+  local pos2id, id2pos = {}, {}
+  local last
+  if not status and next(shadow) == nil then
+    return pos2id, id2pos
+  end
+  repeat
+    local key = lib.marktree_itr_test(iter)
+    local id = tonumber(key.id)
+    local spos = shadow[id]
+    if (key.pos.row ~= spos[1] or key.pos.col ~= spos[2]) then
+      error("invalid pos for "..id..": ("..spos[1]..", "..spos[2]..")")
+    end
+    if count > 0 then
+      if spos[1] < last[1] or (spos[1] == last[1] and spos[2] < last[2]) then
+        error("DISORDER")
+      end
+    end
+    count = count + 1
+    last = spos
+    pos2id[count] = id
+    id2pos[id] = count
+  until not lib.marktree_itr_next(tree, iter)
+  if tablelength(shadow) ~= count then
+    error("missed some keys?")
+  end
+end
+
 describe('marktree', function()
  itp('works', function()
     tree = lib.marktree_new(1)
-    shadow = {}
+    local shadow = {}
     iter = ffi.new("MarkTreeIter[1]")
 
     for i = 1,100 do
       for j = 1,100 do
-        id = lib.marktree_put(tree, j, i)
+        id = tonumber(lib.marktree_put(tree, j, i))
         ok(id > 0)
         eq(nil, shadow[id])
         shadow[id] = {j,i}
       end
     end
+
+    local id2pos, pos2id = shadoworder(tree, shadow, iter)
     if false then
       ss = lib.mt_inspect_rec(tree)
       io.stdout:write(ffi.string(ss))
@@ -37,8 +76,8 @@ describe('marktree', function()
       eq(ipos[1], k.pos.row)
       eq(ipos[2], k.pos.col, ipos[1])
       lib.marktree_itr_next(tree, iter)
+      local k2 = lib.marktree_itr_test(iter)
       -- TODO
-      --local k2 = lib.marktree_itr_test(iter)
     end
  end)
 end)
