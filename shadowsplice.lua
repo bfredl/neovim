63ce2adbbe37a7af13ddbba7c5b89e49ac7401e5
Björn Linse <bjorn.linse@gmail.com>
2020-03-25 12:53:40 +0100

    werk

--- shadowsplice.lua
+++ shadowsplice.lua
@@ -11,6 +11,7 @@ function ssp.start(bufnr)
     bufnr = a.nvim_get_current_buf()
   end
   ssp.bufnr = bufnr
+  ssp.sched = false
   ssp.reset()
   a.nvim_buf_attach(bufnr, false, {on_bytes=function(...)
     ssp.on_bytes(...)
@@ -22,6 +23,7 @@ function ssp.reset()
   local bytes = table.concat(text, '\n') .. '\n'
   ssp.shadow = bytes
   ssp.dirty = false
+  a.nvim_buf_clear_namespace(ssp.bufnr, ssp.ns, 0, -1)
 end
 
 function ssp.on_bytes(_, buf, tick, start_row, start_col, start_byte, old_row, old_col, old_byte, new_row, new_col, new_byte)
@@ -31,10 +33,18 @@ function ssp.on_bytes(_, buf, tick, start_row, start_col, start_byte, old_row, o
   local unknown = string.rep('\255', new_byte)
   local after = string.sub(ssp.shadow, start_byte + old_byte + 1)
   ssp.shadow = before .. unknown .. after
+  if not ssp.sched then
+    vim.schedule(ssp.show)
+    ssp.sched = true
+  end
+
+  vim.schedule(function()
+    luadev.print(vim.inspect{start_row, start_col, start_byte, old_row, old_col, old_byte, new_row, new_col, new_byte})
+  end)
 end
 
 function ssp.sync()
-  local text = meths.buf_get_lines(0, 0, -1, true)
+  local text = a.nvim_buf_get_lines(ssp.bufnr, 0, -1, true)
   local bytes = table.concat(text, '\n') .. '\n'
   for i = 1, string.len(ssp.shadow) do
     local shadowbyte = string.sub(ssp.shadow, i, i)
@@ -47,41 +57,40 @@ function ssp.sync()
 end
 
 function ssp.show()
+  ssp.sched = false
   a.nvim_buf_clear_namespace(ssp.bufnr, ssp.ns, 0, -1)
-  local text = meths.buf_get_lines(ssp.bufnr, 0, -1, true)
+  local text = a.nvim_buf_get_lines(ssp.bufnr, 0, -1, true)
   local bytes = table.concat(text, '\n') .. '\n'
   local line, lastpos = 0, 0
   for i = 1, string.len(ssp.shadow) do
+    local textbyte = string.sub(bytes, i, i)
     if textbyte == '\n' then
       line = line + 1
       lastpos = i
     end
-    local textbyte = string.sub(bytes, i, i)
     local shadowbyte = string.sub(ssp.shadow, i, i)
     if shadowbyte ~= '\255' then
       if textbyte ~= shadowbyte then
-        a.nvim_buf_add_highlight(ssp.bufnr, ssp.ns, "ErrorMsg", line, i-lastpos, i-lastpos+1)
+        pcall(function()
+          a.nvim_buf_add_highlight(ssp.bufnr, ssp.ns, "ErrorMsg", line, i-lastpos-1, i-lastpos)
+          a.nvim_buf_set_virtual_text(ssp.bufnr, ssp.ns, line, {{"ERR", "ErrorMsg"}}, {})
+        end)
       end
     else
-        a.nvim_buf_add_highlight(ssp.bufnr, ssp.ns, "MoreMsg", line, i-lastpos, i-lastpos+1)
+        a.nvim_buf_add_highlight(ssp.bufnr, ssp.ns, "StatusLine", line, i-lastpos, i-lastpos+1)
     end
   end
 end
 
-local a = vim.api
-function thecb(...)
-  local yarg = {...}
-  vim.schedule(function() luadev.print(vim.inspect(yarg)) end)
-  -- args are "nvim_buf_lines_event", 1, 85, 8, 8, { "" }, false
-end
-
-function reg()
-  a.nvim_buf_attach(0, false, {on_bytes=thecb})
-end
-
-reg()
+ssp.reset()
 
 ra = [[
+ssp.start()
+ssp.reset()
+eeeeee
+eeee
+ssp.show()
+ssp.sync()
 bx
 yb
   vim.schedule(function() luadev.print(vim.inspect(yarg)) end)
