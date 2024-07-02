9e525699270d14acc89d97f3affbc2baff55360b
bfredl <bjorn.linse@gmail.com>
2024-07-02 13:45:50 +0200

    refactor(shada): rework msgpack decoding without msgpack-c
    This also makes shada reading slightly faster due to avoiding
    some copying and allocation.
    
    Use keysets to drive decoding of msgpack maps for shada entries.

--- src/nvim/api/private/defs.h
+++ src/nvim/api/private/defs.h
@@ -20,6 +20,8 @@
 # define ArrayOf(...) Array
 # define DictionaryOf(...) Dictionary
 # define Dict(name) KeyDict_##name
+# define DictHash(name) KeyDict_##name##_get_field
+# define DictKey(name)
 #endif
 
 // Basic types
@@ -90,6 +92,8 @@ typedef kvec_t(Object) Array;
 typedef struct key_value_pair KeyValuePair;
 typedef kvec_t(KeyValuePair) Dictionary;
 
+typedef kvec_t(String) StringArray;
+
 typedef enum {
   kObjectTypeNil = 0,
   kObjectTypeBoolean,
@@ -104,6 +108,10 @@ typedef enum {
   kObjectTypeWindow,
   kObjectTypeTabpage,
 } ObjectType;
+
+typedef enum {
+  kUnpackTypeStringArray = -1,
+} UnpackType;
 
 /// Value by which objects represented as EXT type are shifted
 ///
@@ -142,7 +150,7 @@ typedef struct {
 typedef struct {
   char *str;
   size_t ptr_off;
-  ObjectType type;  // kObjectTypeNil == untyped
+  int type;  // ObjectType or UnpackType. kObjectTypeNil == untyped
   int opt_index;
   bool is_hlgroup;
 } KeySetLink;
