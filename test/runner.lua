48c9b6067c1243f68021d648f66c32bf9f329921
bfredl <bjorn.linse@gmail.com>
2026-05-29 10:43:51 +0200

    fix(build.zig): do not depend on b.install_path on configure time
    This is in preparation of zig 0.17 where b.install_path is no
    longer known inside `pub fn build()`.
    
    Builds which should hardcode a runtime path into the binary has to specify it.
    We could use a nlua0 helper to encode the install path at build time but
    I think this is cleaner anyway. (like currently the appimage still
    encodes a temporary build path which is nonsensical, it is better to
    just include none)
    
    Similarily b.args will not be available anymore (there will just
    be an opaque pass-through)

--- test/runner.lua
+++ test/runner.lua
@@ -23,6 +23,9 @@ _G.c_include_path = {}
 while _G.arg[1] and vim.startswith(_G.arg[1], '-I') do
   table.insert(_G.c_include_path, string.sub(table.remove(_G.arg, 1), 3))
 end
+if _G.arg[1] and vim.startswith(_G.arg[1], '-P') then
+  _G.nvim_build_dir = string.sub(table.remove(_G.arg, 1), 3)
+end
 
 local root = repo_root()
 prepend_package_roots({ root, root .. '/test', '.', './test' })
